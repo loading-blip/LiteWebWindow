3be89af5554ed83100e00c518f06e56a4f0bf4b6
Loading-bilp <2650047570@qq.com>
2025-03-11 21:09:45 +0800

    little fix

--- src/components/windowDomain/queryDomain.vue
+++ src/components/windowDomain/queryDomain.vue
@@ -1,20 +1,25 @@
 <script setup>
-
+import $ from 'jquery'
+import { onMounted } from 'vue';
 const props = defineProps({
     data: {
         type: String,
         required: true,
     }
 })
+onMounted(()=>{
+    $('#queryValidate').text(props.data['validate']?"":"尾号错误或未找到订单")
+})
 </script>
 
 <template>
     <h2>查询结果</h2>
     <table id="result" >
-        <tr v-for="item in data">
-            <td v-for="col in item" >{{ col }}</td>
+        <tr v-for="item in props.data['expressNum']">
+            <td>{{ item }}</td>
         </tr>
     </table>
+    <div id="queryValidate"></div>
 </template>
 
 <style scoped lang="scss">
